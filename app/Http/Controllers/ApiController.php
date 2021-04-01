828de4ca32ba36a495b1c891ca6bbf542fb62d74
Rachel Beacham <rachelbeacham17@gmail.com>
2021-04-01 08:40:45 -0700

    revert to original

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,34 +8,5 @@ use App\Models\User;
 
 class ApiController extends Controller
 {
-    public function getAllUsers() {
-        $users = User::get()->toJson();
-        return response($users, 200);
-    }
 
-     public function store(Request $request) {
-        $request->validate([
-            'first_name' => 'required',
-            'last_name'  => 'required',
-            'email'      => 'required',
-        ]);
-        $user = User::create($request->all());
-
-        return response()->json([
-            "message" => "user record created"
-        ], 201);
-    }
-
-    public function update(Request $request, $id) {
-        if (User::where('id', $id)->exists()) {
-            $user->update($request->all());
-            return response()->json([
-                "message" => 'user record updated'
-            ], 200);
-        } else {
-            return response()->json([
-                "message" => 'user not found'
-            ],404);
-        }
-    }
 }
